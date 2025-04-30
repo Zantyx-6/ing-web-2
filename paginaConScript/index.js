c1366ecde12414bf6f170e13ba6eb06c75189c26
Zantyx-6 <santi2006cor@gmail.com>
2025-04-30 09:20:28 -0500

    Add files via upload

---
+++ paginaConScript/index.js
@@ -0,0 +1,16 @@
+let edad = parseInt(prompt("Digite su edad: "))
+
+if (edad < 18 && edad > 0){
+    console.log("Usted es menor de edad.")
+}
+if (edad >= 18){
+    console.log("Usted es mayor de edad.")
+}
+if (edad < 0){
+    console.log("Edad invalida !")
+}
+
+
+for (let i = 0; i < 11; i++){
+    console.log(i)
+}
